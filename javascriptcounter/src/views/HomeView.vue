64b221f8141258bf8a78507a05b156ab618061a6
tboun <tboun@Apple.router.taotronics.com>
2022-09-18 00:35:16 -0700

    first commit

---
+++ javascriptcounter/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+  <div class="counter">
+    0
+  </div>
+  <div class="button">
+    <button>-</button>
+    <button>+</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  }
+
+</script>
